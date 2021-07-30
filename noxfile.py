e4a89600832aac6f5fcb53613516510d6aae5c8b
Michael Twomey <mick@twomeylee.name>
2021-07-30 17:00:47 +0100

    Add a lint step to nox

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,15 @@
 import nox
 
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session()
+def lint(session):
+    session.install(".", "mypy", "black", "isort")
+    session.run("mypy", ".")
+    session.run("isort", "--check", ".")
+    session.run("black", "--check", ".")
+
 
 @nox.session(py=["3.8", "3.9", "3.10"])
 def test(session: nox.Session):
